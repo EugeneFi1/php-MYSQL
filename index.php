3ae982052c2df21d49658005a8fe0e0b41a53e73
EugeneFi1 <51033432+EugeneFi1@users.noreply.github.com>
2020-04-11 18:08:27 +0200

    print accounts table

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
-<?php
-    echo "Hello";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+   <?php include "function.php" ?> 
+   <?php
+        $accounts = getAccounts($link);
+        
+        echo "<pre>";
+            print_r($accounts);
+        echo "</pre>";    
+   ?>
+</body>
+</html>
